51f07a950c92fb376caa4f865580af26f626ba1e
Karl-John Chow <Karl.chow92@gmail.com>
2016-04-15 16:32:47 +0800

    Added multiple entries for StepsController

--- drone_api/src/AppBundle/Builder/ResponseMessageBuilder.php
+++ drone_api/src/AppBundle/Builder/ResponseMessageBuilder.php
@@ -19,10 +19,20 @@ class ResponseMessageBuilder
     private $status;
 
 
-    public function __construct()
+    public function __construct($message = "", $status = "",$data= null,$dataName = "object")
     {
         $this->response = array();
-        $this->response[Strings::$VERSION] = Strings::$VERSION_NUMBER;
+        $this->message = $message;
+        $this->status = $status;
+        if($data){
+            if (self::isMap($data)){
+                $this->addToParams($data,$dataName);
+            }else{
+                foreach ($data as $item){
+                    $this->addToParams($item,$dataName);
+                }
+            }
+        }
     }
 
     public function addToParams($data, $key){
@@ -34,21 +44,18 @@ class ResponseMessageBuilder
         }
 
         if (empty($params)){
-            $this->response[$key] = $data;
+            if (empty($this->response[$key])){
+                $this->response[$key] = $data;
+            }else{
+                $array = $this->response[$key];
+                array_push($array,$data);
+                $this->response[$key] = $array;
+            }
+
         }else{
             array_push($params, $data);
             $this->response[$key]= $params;
         }
-    }
-
-    public function getResponseArray(){
-        $this->response[Strings::$MESSAGE] = $this->message;
-        $this->response[Strings::$STATUS] = $this->status;
-        return $this->response;
-    }
-
-    public function getResponseJSON(){
-        return json_encode($this->getResponseArray());
     }
 
     public function setStatus($status)
@@ -59,6 +66,19 @@ class ResponseMessageBuilder
     public function setMessage($message)
     {
         $this->message = $message;
+    }
+
+    public function getResponseArray($requireVersion = true){
+        if ($requireVersion){
+            $this->response[Strings::$VERSION] = Strings::$VERSION_NUMBER;
+        }
+        $this->response[Strings::$MESSAGE] = $this->message;
+        $this->response[Strings::$STATUS] = $this->status;
+        return $this->response;
+    }
+
+    public function getResponseJSON($requireVersion = true){
+        return json_encode($this->getResponseArray($requireVersion));
     }
 
     public static function isMap(array $array)
